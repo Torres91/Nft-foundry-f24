0528f4cdd5a678ef0862a32a4443abdb8573f22f
Jason <Jason@iMac.com>
2024-01-08 10:15:02 +0200

    Nft project

---
+++ test/uint/DeployMoodNftTest.t.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test,console} from "forge-std/Test.sol";
+import {DeployMoodNft} from "../../script/DeployMoodNft.s.sol";
+
+contract DeployMoodNftTest is Test {
+    DeployMoodNft public deployer; 
+
+    function setUp() public {
+        deployer = new DeployMoodNft();
+
+    }
+
+    function testConvertSvgToUri() public view {
+        string
+            memory expectedUri = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCI+Cjx0ZXh0IHg9IjAiIHk9IjE1IiBmaWxsPSJibGFjayI+SGkhIFlvdXIgYnJvd3NlciBkZWNvZGVkIHRoaXM8L3RleHQ+Cjwvc3ZnPg==";
+        string 
+            memory svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="500" height="500"><text x="0" y="15" fill="black">Hi! Your browser decoded this</text></svg>';
+        string memory actualUri = deployer.svgToImageURI(svg);
+        assert(
+            keccak256(abi.encodePacked(actualUri)) ==
+                keccak256(abi.encodePacked(expectedUri))
+        );
+   
+
+    }
+}
